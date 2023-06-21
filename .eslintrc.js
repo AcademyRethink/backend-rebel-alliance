51d7fa6b5dec9a7af0428b70a69807449673947a
thiagoFirsen <thiago.firsen@rethink.dev>
2023-06-21 13:37:03 -0300

    chore: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
