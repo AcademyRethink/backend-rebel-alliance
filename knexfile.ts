ce50b0933c36b937fc9d57bfcffddb1e239a4489
thiagoFirsen <thiago.firsen@rethink.dev>
2023-06-22 13:10:44 -0300

    chore: ambient config

--- knexfile.ts
+++ knexfile.ts
@@ -2,7 +2,7 @@ import type { Knex } from "knex";
 
 const config: Knex.Config = {
   client: "pg",
-  connection: "",
+  connection: process.env.PG_CONNECTION_STRING,
   migrations: {
     directory: "src/api/database",
   },
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import express, { Express, Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
 const app: Express = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Bem vindo ao Dashboard da Rebel Alliance");
