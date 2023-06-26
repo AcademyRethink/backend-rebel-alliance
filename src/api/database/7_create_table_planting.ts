1025ebb69ee466d0f8940deed8ce11b48037c504
thiagoFirsen <thiago.firsen@rethink.dev>
2023-06-26 13:16:03 -0300

    chore: database config

--- src/api/database/7_create_table_planting.ts
+++ src/api/database/7_create_table_planting.ts
@@ -1,17 +1,21 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("harvest", (table) => {
+  await knex.schema.createTable("planting", (table) => {
     table.increments();
     table.date("date").notNullable();
-    table.float("bags").notNullable();
+    table.integer("saplings").notNullable();
     table.integer("plot_id").notNullable();
     table.foreign("plot_id").references("plot.id");
+    table.integer("stages_id").notNullable();
+    table.foreign("stages_id").references("stages.id");
+    table.string("user").notNullable();
+    table.foreign("user").references("users.cpf_cnpj");
     table.string("farm").notNullable();
     table.foreign("farm").references("farm.cnpj");
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("harvest");
+  await knex.schema.dropTable("planting");
 }
--- src/api/database/8_create_table_harvest.ts
+++ src/api/database/8_create_table_harvest.ts
@@ -1,18 +1,19 @@
 import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable("planting", (table) => {
+  await knex.schema.createTable("harvest", (table) => {
     table.increments();
     table.date("date").notNullable();
-    table.integer("saplings").notNullable();
+    table.float("bags").notNullable();
     table.integer("plot_id").notNullable();
     table.foreign("plot_id").references("plot.id");
-    table.string("status").notNullable();
+    table.string("user").notNullable();
+    table.foreign("user").references("users.cpf_cnpj");
     table.string("farm").notNullable();
     table.foreign("farm").references("farm.cnpj");
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("planting");
+  await knex.schema.dropTable("harvest");
 }
