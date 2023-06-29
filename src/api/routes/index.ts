0d9fb558e1b26fc186bc379c69517c284881b3af
ribYuriRethink <yuri.maycon@rethink.dev>
2023-06-29 16:52:57 -0300

    feat: initial structure

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { harvestRouter } from "./harvestRoutes";
+import { router as plotRoutes } from "./plots";
 
 const router: Router = Router();
 
 router.use("/harvests", harvestRouter);
+router.use("/plots", plotRoutes);
 
 export { router };
