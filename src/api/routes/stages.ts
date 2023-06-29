8262eb4692eb030dd21db695191a8ee2445d1a34
thiagoFirsen <thiago.firsen@rethink.dev>
2023-06-29 15:10:48 -0300

    feat: create middleware for stage path validator by culture

--- src/api/routes/stages.ts
+++ src/api/routes/stages.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import stagesController from "../controllers/stagesController";
+import middleware from "../middlewares/stagesDataValidator";
 
 const router: Router = Router();
 
 router.get("/", stagesController.index);
-router.get("/:id", stagesController.show);
+router.get(
+  "/:id",
+  middleware.stagesPathValidatorByCulture,
+  stagesController.show
+);
 
 export { router };
