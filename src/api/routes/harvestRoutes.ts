8ef3fcd013ccc2b17f414eb876677a3f990b5fb6
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-07-04 15:02:30 -0300

    fix/ dataValidator os harvest (incorrect file name)

--- src/api/routes/harvestRoutes.ts
+++ src/api/routes/harvestRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import harvestController from "../controllers/harvestController";
-import dataValidator from "../middlewares/dataValidator";
+import dataValidator from "../middlewares/harvestsDataValidator";
 
 const harvestRouter: Router = Router();
 
