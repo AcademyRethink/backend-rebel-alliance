bbd68c7ed264682049df229386145f99c35125bd
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-04 13:51:05 -0300

    refactor: after code review

--- src/tests/stages.test.ts
+++ src/tests/stages.test.ts
@@ -4,20 +4,20 @@ import { describe, expect, jest } from "@jest/globals";
 import { stageData } from "./mockStages";
 import { StagesWithName } from "../types/stagesTypes";
 describe("Stages Tests", () => {
-  it("Read All Stagees", async () => {
+  it("Read All Stages", async () => {
     jest
       .spyOn(stagesRepository, "selectAllStages")
       .mockResolvedValueOnce([stageData]);
     const result: StagesWithName[] = await stagesService.getAllStages();
     expect(result).toMatchObject([stageData]);
   });
-  it("Stages not found", async () => {
+  it("Culture not found", async () => {
     jest.spyOn(stagesRepository, "selectAllStages").mockResolvedValueOnce([]);
     try {
       await stagesService.getAllStages();
     } catch (error) {
       expect(error).toMatchObject({
-        message: "Stages not found",
+        message: "Culture not found",
         status: 400,
       });
     }
@@ -31,7 +31,7 @@ describe("Stages Tests", () => {
     );
     expect(result).toMatchObject([stageData]);
   });
-  it("Stages not found - All Stages of a culture", async () => {
+  it("Culture not found - All Stages of a culture", async () => {
     jest
       .spyOn(stagesRepository, "selectAllStagesOfACulture")
       .mockResolvedValueOnce([]);
@@ -39,7 +39,7 @@ describe("Stages Tests", () => {
       await stagesService.getAllStagesOfACulture(0);
     } catch (error) {
       expect(error).toMatchObject({
-        message: "Stages not found",
+        message: "Culture not found",
         status: 400,
       });
     }
