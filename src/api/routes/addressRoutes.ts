a3529b97137177fc58ee4608525209981477f392
Leticia-Guimaraes <124922074+Leticia-Guimaraes@users.noreply.github.com>
2023-07-06 12:08:59 -0300

    Chore/ Alteração nas rotas

--- src/api/routes/addressRoutes.ts
+++ src/api/routes/addressRoutes.ts
@@ -4,9 +4,9 @@ import addressController from "../controllers/addressController";
 const addressRoutes: Router = Router();
 
 addressRoutes.get("/", addressController.showAllAddresses);
-addressRoutes.get("/id/:addressid", addressController.showById);
+addressRoutes.get("/:addressid", addressController.showById);
 addressRoutes.get("/cep/:addresscep", addressController.showByCep);
 addressRoutes.get("/street/:addressstreet", addressController.showByStreet);
-addressRoutes.delete("/id/:addressid", addressController.remove);
+addressRoutes.delete("/:addressid", addressController.remove);
 
 export { addressRoutes };
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -14,7 +14,7 @@ router.use("/weather", weatherRoutes);
 router.use("/harvests", harvestRouter);
 router.use("/users", userRoutes);
 router.use("/farms", farmRoutes);
-router.use("/address", addressRoutes);
+router.use("/addresses", addressRoutes);
 router.use("/plots", plotRoutes);
 router.use("/plantings", plantingRoutes);
 router.use("/stages", stagesRoutes);
