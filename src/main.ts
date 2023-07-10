72db3a38b20060704849da25d9904368e5a69fbc
DuarteJoseGeraldo <124914532+DuarteJoseGeraldo@users.noreply.github.com>
2023-07-10 15:36:06 -0300

    fix/ main tava com erro

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from "express";
+import { router } from "./api/routes";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { errorHandler } from "./api/middlewares/errorHandler";
 
 dotenv.config();
 const app: Express = express();
@@ -10,6 +12,8 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Bem vindo ao Dashboard da Rebel Alliance");
 });
+app.use("/", router);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Ta funcionando");
