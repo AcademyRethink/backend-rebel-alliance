8f29fd685eecf374540129c7fb48d430da8eb571
thiagoFirsen <thiago.firsen@rethink.dev>
2023-07-10 12:16:53 -0300

    tests: user unit tests

--- src/tests/mockUsers.ts
+++ src/tests/mockUsers.ts
@@ -4,7 +4,7 @@ export const mockedUser = {
   name: "Leticia 2",
   celphone: "97343515442",
   email: "leticia@mail.com.br",
-  password: "$2b$10$SnoLNSXTjZxYIZDZX9m4AeBnjZxCETwfKw7gt3ZojarG.gn8l9WY.",
+  password: "senha1234",
   userType: "Administrador",
   farm_id: 25,
 };
@@ -17,4 +17,15 @@ export const mockedUserWithoutPassword = {
   email: "leticia@mail.com.br",
   userType: "Administrador",
   farm_id: 25,
+};
+
+export const mockedUserWithCryptoPassword = {
+  id: 10,
+  cpf_cnpj: "095792196992",
+  name: "Thiago",
+  celphone: "99842535111",
+  email: "thiago@admin.com",
+  password: "$2b$10$PpkJMK6995DJ939qsxE.j.CmpXHk8jRFDbkCSgU7vjgwuWCNmdxYi",
+  userType: "Administrador",
+  farm_id: 1,
 };
